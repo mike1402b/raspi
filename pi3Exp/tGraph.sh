8b4d371fba60b17b5eef22c23613e73cf2be3b80
mike1402@gmx.at <mike1402@gmx.at>
2017-06-28 14:03:27 +0200

    weekly min max added

--- pi3Exp/tGraph.sh
+++ pi3Exp/tGraph.sh
@@ -1,4 +1,5 @@
 
+
 rrdtool graph tDay.png  -s 'now - 1 day' -e 'now' \
   DEF:temp0=temperature.rrd:temp0:AVERAGE \
   LINE2:temp0#00FF00:Aussen
@@ -6,9 +7,12 @@ cp tDay.png /var/www/html/
 
 
 rrdtool graph tWeek.png  -s 'now - 1 week' -e 'now' \
-  DEF:temp0=temperature.rrd:temp0:AVERAGE \
-  LINE2:temp0#00FF00:Aussen
-
+  DEF:t0=temperature.rrd:temp0:MIN \
+  LINE1:t0#0000FF:AussenMin \
+  DEF:t1=temperature.rrd:temp0:AVERAGE \
+  LINE2:t1#00FF00:AussenAvg \
+  DEF:t2=temperature.rrd:temp0:MAX \
+  LINE2:t2#FF0000:AussenMax
 cp tWeek.png /var/www/html/
 
 
@@ -19,7 +23,6 @@ rrdtool graph tMonth.png  -s 'now - 1 month' -e 'now' \
   LINE2:t1#00FF00:AussenAvg \
   DEF:t2=temperature.rrd:temp0:MAX \
   LINE2:t2#FF0000:AussenMax
-
 cp tMonth.png /var/www/html/
 
 
