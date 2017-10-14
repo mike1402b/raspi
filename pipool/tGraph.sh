14fd9fabeaad0334e8d92b0ff800e74bb9a83dfc
mike1402 <mike1402@gmx.at>
2017-10-14 17:46:25 +0000

    all graph better

--- pipool/tGraph.sh
+++ pipool/tGraph.sh
@@ -36,14 +36,14 @@ rrdtool graph tDayAll.png  -s 'now - 1 day' -e 'now' \
   DEF:t0=temp.rrd:t0:AVERAGE \
   LINE2:t0#FF0000:pi \
   DEF:t1=temp.rrd:t1:AVERAGE \
-  LINE2:t1#0000FF:poolOben \
+  LINE2:t1#0000FF:poolO \
   DEF:t2=temp.rrd:t2:AVERAGE \
-  LINE2:t2#0000F1:poolUnten \
+  LINE2:t2#0000DD:poolU \
   DEF:t3=temp.rrd:t3:AVERAGE \
-  LINE2:t3#00FF00:erde tief \
-  DEF:t4=temp.rrd:t3:AVERAGE \
-  LINE2:t4#00F100:erde oben \
-  DEF:t5=temp.rrd:t3:AVERAGE \
-  LINE2:t5#A0E100: kiste oben 
+  LINE2:t3#00FF00:erdeU \
+  DEF:t4=temp.rrd:t4:AVERAGE \
+  LINE2:t4#00DD00:erdeO \
+  DEF:t5=temp.rrd:t5:AVERAGE \
+  LINE2:t5#A0FF00:kisteOben 
   
 cp tDayAll.png /var/www/html/
