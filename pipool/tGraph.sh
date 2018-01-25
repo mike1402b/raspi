9f89f0afb39d56d9347254669c3f9b955658393d
Michael Hewarth <michael.hewarth@isiqiri.com>
2018-01-25 16:48:18 +0100

    aussen und kisteOben vertauscht

--- pipool/tGraph.sh
+++ pipool/tGraph.sh
@@ -5,12 +5,12 @@ rrdtool graph tDay.png  -s 'now - 1 day' -e 'now' -w 400 -h 200 \
   LINE2:t0#FF0000:pi \
   DEF:t8=temp.rrd:t8:AVERAGE \
   LINE2:t8#AA0000:solar \
-  DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:aussen \
+  DEF:t6=temp.rrd:t6:AVERAGE \
+  LINE2:t6#ffc0c0:Aussen \
   DEF:t1=temp.rrd:t1:AVERAGE \
-  LINE2:t1#0000FF:poolU \
+  LINE2:t1#0000FF:poolUnten \
   DEF:t2=temp.rrd:t2:AVERAGE \
-  LINE2:t2#000077:poolO 
+  LINE2:t2#000077:poolOben 
   
 cp tDay.png /var/www/html/
 
@@ -20,7 +20,7 @@ rrdtool graph tWeek.png  -s 'now - 1 week' -e 'now' -w 400 -h 200 \
   DEF:t8=temp.rrd:t8:AVERAGE \
   LINE2:t8#AA0000:solar \
   DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:aussen \
+  LINE2:t7#00FFAA:kisteOben \
   DEF:t1=temp.rrd:t1:AVERAGE \
   LINE2:t1#0000FF:poolU \
   DEF:t2=temp.rrd:t2:AVERAGE \
@@ -65,11 +65,11 @@ rrdtool graph tDayAll.png  -s 'now - 1 day' -e 'now' -w 400 -h 200 \
   DEF:t4=temp.rrd:t4:AVERAGE \
   LINE2:t4#009900:erdeO \
   DEF:t5=temp.rrd:t5:AVERAGE \
-  LINE2:t5#FF0000:kisteU \
+  LINE2:t5#FF0000:kisteUnten \
   DEF:t6=temp.rrd:t6:AVERAGE \
-  LINE2:t6#ffc0c0:kisteO \
+  LINE2:t6#ffc0c0:aussen \
   DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:aussen 
+  LINE2:t7#00FFAA:kisteOben 
   
 cp tDayAll.png /var/www/html/
 
