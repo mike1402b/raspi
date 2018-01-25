9d94f1a6d0b033ea0f20337c2970c38b5fccb77a
Michael Hewarth <michael.hewarth@isiqiri.com>
2018-01-25 16:43:29 +0100

    aussen auch zu diag 1 alle zeiten

--- pipool/tGraph.sh
+++ pipool/tGraph.sh
@@ -5,6 +5,8 @@ rrdtool graph tDay.png  -s 'now - 1 day' -e 'now' -w 400 -h 200 \
   LINE2:t0#FF0000:pi \
   DEF:t8=temp.rrd:t8:AVERAGE \
   LINE2:t8#AA0000:solar \
+  DEF:t7=temp.rrd:t7:AVERAGE \
+  LINE2:t7#00FFAA:aussen \
   DEF:t1=temp.rrd:t1:AVERAGE \
   LINE2:t1#0000FF:poolU \
   DEF:t2=temp.rrd:t2:AVERAGE \
@@ -17,6 +19,8 @@ rrdtool graph tWeek.png  -s 'now - 1 week' -e 'now' -w 400 -h 200 \
   LINE2:t0#FF0000:pi \
   DEF:t8=temp.rrd:t8:AVERAGE \
   LINE2:t8#AA0000:solar \
+  DEF:t7=temp.rrd:t7:AVERAGE \
+  LINE2:t7#00FFAA:aussen \
   DEF:t1=temp.rrd:t1:AVERAGE \
   LINE2:t1#0000FF:poolU \
   DEF:t2=temp.rrd:t2:AVERAGE \
@@ -29,10 +33,12 @@ rrdtool graph tMonth.png  -s 'now - 1 month' -e 'now' -w 400 -h 200 \
   LINE2:t0#FF0000:pi \
   DEF:t8=temp.rrd:t8:AVERAGE \
   LINE2:t8#AA0000:solar \
+  DEF:t7=temp.rrd:t7:AVERAGE \
+  LINE2:t7#00FFAA:aussen \
   DEF:t1=temp.rrd:t1:AVERAGE \
   LINE2:t1#0000FF:poolU \
   DEF:t2=temp.rrd:t2:AVERAGE \
-  LINE2:t2#000077:poolO 
+  LINE2:t2#000077:poolO  
 
 cp tMonth.png /var/www/html/
 
