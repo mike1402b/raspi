0170c2e9004d749038149b90cd35e71904ca4ae0
Michael Hewarth <michael.hewarth@isiqiri.com>
2018-01-25 16:53:11 +0100

    vereinheitlicht

--- pipool/tGraph.sh
+++ pipool/tGraph.sh
@@ -8,9 +8,9 @@ rrdtool graph tDay.png  -s 'now - 1 day' -e 'now' -w 400 -h 200 \
   DEF:t6=temp.rrd:t6:AVERAGE \
   LINE2:t6#ffc0c0:Aussen \
   DEF:t1=temp.rrd:t1:AVERAGE \
-  LINE2:t1#0000FF:poolUnten \
+  LINE2:t1#0000FF:poolU \
   DEF:t2=temp.rrd:t2:AVERAGE \
-  LINE2:t2#000077:poolOben 
+  LINE2:t2#000077:poolO 
   
 cp tDay.png /var/www/html/
 
@@ -19,8 +19,8 @@ rrdtool graph tWeek.png  -s 'now - 1 week' -e 'now' -w 400 -h 200 \
   LINE2:t0#FF0000:pi \
   DEF:t8=temp.rrd:t8:AVERAGE \
   LINE2:t8#AA0000:solar \
-  DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:kisteOben \
+  DEF:t6=temp.rrd:t6:AVERAGE \
+  LINE2:t6#ffc0c0:Aussen \
   DEF:t1=temp.rrd:t1:AVERAGE \
   LINE2:t1#0000FF:poolU \
   DEF:t2=temp.rrd:t2:AVERAGE \
@@ -33,12 +33,12 @@ rrdtool graph tMonth.png  -s 'now - 1 month' -e 'now' -w 400 -h 200 \
   LINE2:t0#FF0000:pi \
   DEF:t8=temp.rrd:t8:AVERAGE \
   LINE2:t8#AA0000:solar \
-  DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:aussen \
+  DEF:t6=temp.rrd:t6:AVERAGE \
+  LINE2:t6#ffc0c0:Aussen \
   DEF:t1=temp.rrd:t1:AVERAGE \
   LINE2:t1#0000FF:poolU \
   DEF:t2=temp.rrd:t2:AVERAGE \
-  LINE2:t2#000077:poolO  
+  LINE2:t2#000077:poolO 
 
 cp tMonth.png /var/www/html/
 
@@ -47,8 +47,8 @@ rrdtool graph tYear.png  -s 'now - 1 year' -e 'now' -w 400 -h 200 \
   LINE2:t0#FF0000:pi \
   DEF:t8=temp.rrd:t8:AVERAGE \
   LINE2:t8#AA0000:solar \
-  DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:aussen \
+  DEF:t6=temp.rrd:t6:AVERAGE \
+  LINE2:t6#ffc0c0:Aussen \
   DEF:t1=temp.rrd:t1:AVERAGE \
   LINE2:t1#0000FF:poolU \
   DEF:t2=temp.rrd:t2:AVERAGE \
@@ -64,12 +64,12 @@ rrdtool graph tDayAll.png  -s 'now - 1 day' -e 'now' -w 400 -h 200 \
   LINE2:t3#00FF00:erdeU \
   DEF:t4=temp.rrd:t4:AVERAGE \
   LINE2:t4#009900:erdeO \
-  DEF:t5=temp.rrd:t5:AVERAGE \
-  LINE2:t5#FF0000:kisteUnten \
   DEF:t6=temp.rrd:t6:AVERAGE \
-  LINE2:t6#ffc0c0:aussen \
+  LINE2:t6#00FFAA:aussen \
+  DEF:t5=temp.rrd:t5:AVERAGE \
+  LINE2:t5#FF0000:kisteU \
   DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:kisteOben 
+  LINE2:t7#FFCC00:kisteO 
   
 cp tDayAll.png /var/www/html/
 
@@ -78,15 +78,13 @@ rrdtool graph tWeekAll.png  -s 'now - 1 week' -e 'now' -w 400 -h 200 \
   DEF:t3=temp.rrd:t3:AVERAGE \
   LINE2:t3#00FF00:erdeU \
   DEF:t4=temp.rrd:t4:AVERAGE \
-  LINE2:t4#001100:erdeO \
-  DEF:t5=temp.rrd:t5:AVERAGE \
-  LINE2:t5#AAFF00:kisteU \
+  LINE2:t4#009900:erdeO \
   DEF:t6=temp.rrd:t6:AVERAGE \
-  LINE2:t6#55AA00:kisteO \
+  LINE2:t6#00FFAA:aussen \
+  DEF:t5=temp.rrd:t5:AVERAGE \
+  LINE2:t5#FF0000:kisteU \
   DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:aussen \
-  DEF:t8=temp.rrd:t8:AVERAGE \
-  LINE2:t8#AA0000:solar 
+  LINE2:t7#FFCC00:kisteO 
   
 cp tWeekAll.png /var/www/html/
 
@@ -94,15 +92,13 @@ rrdtool graph tMonthAll.png  -s 'now - 1 month' -e 'now' -w 400 -h 200 \
   DEF:t3=temp.rrd:t3:AVERAGE \
   LINE2:t3#00FF00:erdeU \
   DEF:t4=temp.rrd:t4:AVERAGE \
-  LINE2:t4#001100:erdeO \
-  DEF:t5=temp.rrd:t5:AVERAGE \
-  LINE2:t5#AAFF00:kisteU \
+  LINE2:t4#009900:erdeO \
   DEF:t6=temp.rrd:t6:AVERAGE \
-  LINE2:t6#55AA00:kisteO \
+  LINE2:t6#00FFAA:aussen \
+  DEF:t5=temp.rrd:t5:AVERAGE \
+  LINE2:t5#FF0000:kisteU \
   DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:aussen \
-  DEF:t8=temp.rrd:t8:AVERAGE \
-  LINE2:t8#AA0000:solar 
+  LINE2:t7#FFCC00:kisteO 
   
 cp tMonthAll.png /var/www/html/
 
@@ -110,14 +106,12 @@ rrdtool graph tYearAll.png  -s 'now - 1 year' -e 'now' -w 400 -h 200 \
   DEF:t3=temp.rrd:t3:AVERAGE \
   LINE2:t3#00FF00:erdeU \
   DEF:t4=temp.rrd:t4:AVERAGE \
-  LINE2:t4#001100:erdeO \
-  DEF:t5=temp.rrd:t5:AVERAGE \
-  LINE2:t5#AAFF00:kisteU \
+  LINE2:t4#009900:erdeO \
   DEF:t6=temp.rrd:t6:AVERAGE \
-  LINE2:t6#55AA00:kisteO \
+  LINE2:t6#00FFAA:aussen \
+  DEF:t5=temp.rrd:t5:AVERAGE \
+  LINE2:t5#FF0000:kisteU \
   DEF:t7=temp.rrd:t7:AVERAGE \
-  LINE2:t7#00FFAA:aussen \
-  DEF:t8=temp.rrd:t8:AVERAGE \
-  LINE2:t8#AA0000:solar 
+  LINE2:t7#FFCC00:kisteO 
   
 cp tYearAll.png /var/www/html/
