183bb687f447032ffe0c21c19a10c27f72be57d0
mike1402 <mike1402@gmx.at>
2018-06-27 11:38:54 +0200

    Webserver a Files Generieren statt t (TDay von mike1402 ...)

--- pi3Exp/aGraph.sh
+++ pi3Exp/aGraph.sh
@@ -1,35 +1,35 @@
 
-rrdtool graph tDay.png  -s 'now - 1 day' -e 'now' \
+rrdtool graph aDay.png  -s 'now - 1 day' -e 'now' \
   DEF:temp0=temperature.rrd:temp0:AVERAGE \
-  LINE2:temp0#00FF00:Aussen
-cp tDay.png /var/www/html/
+  LINE2:temp0#00FF00:Avg
+cp aDay.png /var/www/html/
 
 
-rrdtool graph tWeek.png  -s 'now - 1 week' -e 'now' \
+rrdtool graph aWeek.png  -s 'now - 1 week' -e 'now' \
   DEF:t0=temperature.rrd:temp0:MIN \
-  LINE1:t0#0000FF:AussenMin \
+  LINE1:t0#0000FF:Min \
   DEF:t1=temperature.rrd:temp0:AVERAGE \
-  LINE2:t1#00FF00:AussenAvg \
+  LINE2:t1#00FF00:Avg \
   DEF:t2=temperature.rrd:temp0:MAX \
-  LINE2:t2#FF0000:AussenMax
-cp tWeek.png /var/www/html/
+  LINE2:t2#FF0000:Max
+cp aWeek.png /var/www/html/
 
 
-rrdtool graph tMonth.png  -s 'now - 1 month' -e 'now' \
+rrdtool graph aMonth.png  -s 'now - 1 month' -e 'now' \
   DEF:t0=temperature.rrd:temp0:MIN \
-  LINE1:t0#0000FF:AussenMin \
+  LINE1:t0#0000FF:Min \
   DEF:t1=temperature.rrd:temp0:AVERAGE \
-  LINE2:t1#00FF00:AussenAvg \
+  LINE2:t1#00FF00:Avg \
   DEF:t2=temperature.rrd:temp0:MAX \
-  LINE2:t2#FF0000:AussenMax
-cp tMonth.png /var/www/html/
+  LINE2:t2#FF0000:Max
+cp aMonth.png /var/www/html/
 
 
-rrdtool graph tYear.png  -s 'now - 1 year' -e 'now' \
+rrdtool graph aYear.png  -s 'now - 1 year' -e 'now' \
   DEF:t0=temperature.rrd:temp0:MIN \
-  LINE1:t0#0000FF:AussenMin \
+  LINE1:t0#0000FF:Min \
   DEF:t1=temperature.rrd:temp0:AVERAGE \
-  LINE2:t1#00FF00:AussenAvg \
+  LINE2:t1#00FF00:Avg \
   DEF:t2=temperature.rrd:temp0:MAX \
-  LINE2:t2#FF0000:AussenMax
-cp tYear.png /var/www/html/
+  LINE2:t2#FF0000:Max
+cp aYear.png /var/www/html/
