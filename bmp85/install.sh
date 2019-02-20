8827f75873c8cc130a3edf60a4d4ba004ff07a0b
Michael Hewarth <michael.hewarth@gmx.at>
2019-02-20 15:54:48 +0100

    index.html und apache zu install hinzugefuegt

--- bmp85/install.sh
+++ bmp85/install.sh
@@ -1,2 +1,2 @@
 echo "installiere librrds-perl rrdtool python-smbus i2c-tools"
-sudo apt-get install librrds-perl rrdtool python-rrdtool  python-smbus i2c-tools
+sudo apt-get install librrds-perl rrdtool python-rrdtool  python-smbus i2c-tools apache2
