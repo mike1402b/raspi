95ffe9a7d4affe6b4bc9cf0e143240e4b7d313fe
mike1402b <mike1402@gmx.at>
2019-02-27 18:38:32 +0100

    Update ConfigGit.sh

--- allgemein/ConfigGit.sh
+++ allgemein/ConfigGit.sh
@@ -1,2 +1,4 @@
 git config --global user.email "mike1402@gmx.at"
 git config --global user.name "mike1402b"
+git config --local user.email "mike1402@gmx.at"
+git config --local user.name "mike1402b"
