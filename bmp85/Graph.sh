ea975a06b9aedc908333ee92e73be66ae9329a20
mike1402b <mike1402@gmx.at>
2019-02-27 18:56:28 +0100

    Update Graph.sh

--- bmp85/Graph.sh
+++ bmp85/Graph.sh
@@ -10,4 +10,7 @@ rrdtool graph Week.png  -s 'now - 1 week' -e 'now' \
   LINE2:t2#FF0000:Luftdruck 
 cp Week.png /var/www/html/
 
-
+rrdtool graph Week.png  -s 'now - 1 month' -e 'now' \
+  DEF:t2=bmp.rrd:t1:AVERAGE \
+  LINE2:t2#FF0000:Luftdruck 
+cp Month.png /var/www/html/
