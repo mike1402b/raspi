93c0a3e372e51abb4e9de38b79c4d442a27b445b
mike1402b <mike1402@gmx.at>
2019-04-10 16:39:35 +0200

    Update aGraph.sh
    aGraph von temperature.rdd auf Solar.rdd umgewandelt

--- pi3Exp/aGraph.sh
+++ pi3Exp/aGraph.sh
@@ -1,12 +1,12 @@
 
 rrdtool graph aDay.png  -s 'now - 1 day' -e 'now' \
-  DEF:temp0=temperature.rrd:temp0:AVERAGE \
+  DEF:temp0=solar.rrd:temp0:AVERAGE \
   LINE2:temp0#00FF00:Avg
 cp aDay.png /var/www/html/
 
 
 rrdtool graph aWeek.png  -s 'now - 1 week' -e 'now' \
-  DEF:t0=temperature.rrd:temp0:MIN \
+  DEF:t0=solar.rrd:temp0:MIN \
   LINE1:t0#0000FF:Min \
   DEF:t1=temperature.rrd:temp0:AVERAGE \
   LINE2:t1#00FF00:Avg \
@@ -16,7 +16,7 @@ cp aWeek.png /var/www/html/
 
 
 rrdtool graph aMonth.png  -s 'now - 1 month' -e 'now' \
-  DEF:t0=temperature.rrd:temp0:MIN \
+  DEF:t0=solar.rrd:temp0:MIN \
   LINE1:t0#0000FF:Min \
   DEF:t1=temperature.rrd:temp0:AVERAGE \
   LINE2:t1#00FF00:Avg \
@@ -26,7 +26,7 @@ cp aMonth.png /var/www/html/
 
 
 rrdtool graph aYear.png  -s 'now - 1 year' -e 'now' \
-  DEF:t0=temperature.rrd:temp0:MIN \
+  DEF:t0=solar.rrd:temp0:MIN \
   LINE1:t0#0000FF:Min \
   DEF:t1=temperature.rrd:temp0:AVERAGE \
   LINE2:t1#00FF00:Avg \
