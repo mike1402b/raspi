2cd40da00332c909ed57266918c6847d4e2ae21c
mike1402b <mike1402@gmx.at>
2019-06-10 08:01:09 +0200

    alle realy aus

--- pipool/gpio.py
+++ pipool/gpio.py
@@ -3,7 +3,18 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
  
-RELAIS_1_GPIO = 23
-GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
-GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # aus
-GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # an
+R1 = 23
+R2 = 24
+R3 = 25
+R4 = 16
+GPIO.setup(R1, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.output(R1, GPIO.HIGH) # aus
+
+GPIO.setup(R2, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.output(R2, GPIO.HIGH) # aus
+
+GPIO.setup(R3, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.output(R3, GPIO.HIGH) # aus
+
+GPIO.setup(R4, GPIO.OUT) # GPIO Modus zuweisen
+GPIO.output(R4, GPIO.HIGH) # aus
