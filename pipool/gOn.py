0317fd81ce62cf2338abfaefd622b8ac714aab51
mike1402 <mike1402@gmx.at>
2019-06-11 07:22:27 +0200

    gOn gOff mit pin nummer als parameter

--- pipool/gOn.py
+++ pipool/gOn.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-
+import sys
+print "This is the name of the script: ", sys.argv[0]
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
  
-RELAIS_1_GPIO = 23
+RELAIS_1_GPIO = int(sys.argv[1])
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
 #GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # aus
 GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # an
