ffe0dc120c77d13a9a6d0dbcf9a175fc32e7e912
mike1402 <mike1402@gmx.at>
2020-04-12 10:17:40 +0200

    backup

---
+++ pipool/pumpOff.sh
@@ -0,0 +1,2 @@
+gpio -g mode 23 out
+gpio -g write 23 1 #invers zu relay
--- pipool/pumpOn.py
+++ pipool/pumpOn.py
@@ -5,5 +5,5 @@ GPIO.setmode(GPIO.BCM) # GPIO Nummern statt Board Nummern
  
 RELAIS_1_GPIO = 23
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Modus zuweisen
-GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # aus
-#GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # an
+#GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # aus
+GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # an
