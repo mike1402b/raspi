9015985ee10280a350b33c208abb8d5ad33e6c55
michael hewarth <michael.hewarth@outlook.com>
2020-04-14 21:40:35 +0200

    gpio 18,24 und 25 schalten

---
+++ pipool/piw/gpio18.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width" />
+<title>Schalter</title>
+</head>
+GPIO 18 schalten:
+<form method="get" action="pool.php">
+<input type="submit" value="Licht ein" name="Lichtein">
+<input type="submit" value="Licht aus" name="Lichtaus">
+</form>
+<?php
+$modeon18 = exec("gpio -g mode 18 out");
+if(isset($_GET["Lichtein"])){
+$val = exec("gpio -g write 18 1");
+echo "Licht an";
+}
+else if(isset($_GET["Lichtaus"])){
+$val = exec("gpio -g write 18 0");
+echo "Licht aus";
+}
+
+echo "<hr/>";
+$val = exec("gpio -g read 18");
+echo " gpio18 = $val ";
+echo " <hr />"
+
+
+?>
+</body>
+</html>
---
+++ pipool/piw/gpio25.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width" />
+<title>Schalter</title>
+</head>
+GPIO 25 schalten:
+<form method="get" action="pool.php">
+<input type="submit" value="Licht ein" name="Lichtein">
+<input type="submit" value="Licht aus" name="Lichtaus">
+</form>
+<?php
+$modeon18 = exec("gpio -g mode 25 out");
+if(isset($_GET["Lichtein"])){
+$val = exec("gpio -g write 25 1");
+echo "Licht an";
+}
+else if(isset($_GET["Lichtaus"])){
+$val = exec("gpio -g write 25 0");
+echo "Licht aus";
+}
+
+echo "<hr/>";
+$val = exec("gpio -g read 25");
+echo " gpio25 = $val ";
+echo " <hr />"
+
+
+?>
+</body>
+</html>
