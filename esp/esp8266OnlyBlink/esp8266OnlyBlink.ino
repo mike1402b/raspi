6acd56dd47ad7d77105eba8e963107309cbec16a
mike1402b <mike1402@gmx.at>
2022-08-04 14:55:58 +0200

    esp8266 onlyBlink mit BoardSelect Anleitung

---
+++ esp/esp8266OnlyBlink/esp8266OnlyBlink.ino
@@ -0,0 +1,17 @@
+/*
+wichtig: Werkzeuge/Board/Esp8266/Generic ESP8266 Board
+*/
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  // but actually the LED is on; this is because
+  // it is active low on the ESP-01)
+  delay(500);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(500);                      // Wait for two seconds (to demonstrate the active low LED)
+}
