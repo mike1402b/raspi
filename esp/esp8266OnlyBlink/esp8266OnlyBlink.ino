d010b077268fa0e7e8539923273935cabf46d3c5
mike1402b <mike1402@gmx.at>
2022-08-04 17:31:37 +0200

    esp8266 AdafruitIO Cloud Upload

--- esp/esp8266OnlyBlink/esp8266OnlyBlink.ino
+++ esp/esp8266OnlyBlink/esp8266OnlyBlink.ino
@@ -1,5 +1,6 @@
 /*
 wichtig: Werkzeuge/Board/Esp8266/Generic ESP8266 Board
+dann richtigen COM Port auswählen
 */
 
 void setup() {
@@ -11,7 +12,7 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
   // but actually the LED is on; this is because
   // it is active low on the ESP-01)
-  delay(500);                      // Wait for a second
+  delay(250);                      // Wait for a second
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(500);                      // Wait for two seconds (to demonstrate the active low LED)
+  delay(250);                      // Wait for two seconds (to demonstrate the active low LED)
 }
